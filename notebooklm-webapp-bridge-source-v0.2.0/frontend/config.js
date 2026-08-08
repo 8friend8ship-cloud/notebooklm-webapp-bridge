b2279567cce71dda2467602a54f2d7209f69c74d
8friend8ship-cloud <8friend8ship@hanmail.net>
2026-08-08 15:05:36 +0900

    Configure NotebookLM bridge Content OS OAuth client

--- notebooklm-webapp-bridge-source-v0.2.0/frontend/config.js
+++ notebooklm-webapp-bridge-source-v0.2.0/frontend/config.js
@@ -1,5 +1,5 @@
 window.NLM_BRIDGE_CONFIG = {
-  GOOGLE_CLIENT_ID: "REPLACE_WITH_GOOGLE_OAUTH_WEB_CLIENT_ID",
+  GOOGLE_CLIENT_ID: "42793942510-6i4s5u15npqeh676ih7bhdg91g23rl7j.apps.googleusercontent.com",
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbynWKaVwG1SRE6uWJ6d4r0Q5wEvKbB5foIuphQBGDwi8P2r2qaP6K0FRAV8krr9R70P/exec",
   EXTENSION_ID: "kkbmofmmonolffejdkfnpjdckahlefob"
 };
