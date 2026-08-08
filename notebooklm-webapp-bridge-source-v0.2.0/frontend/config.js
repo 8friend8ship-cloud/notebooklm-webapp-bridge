660fab908c23ec181a534ebee66b68bbbb1c1fbe
8friend8ship-cloud <8friend8ship@hanmail.net>
2026-08-08 15:02:46 +0900

    Configure NotebookLM bridge Apps Script endpoint

--- notebooklm-webapp-bridge-source-v0.2.0/frontend/config.js
+++ notebooklm-webapp-bridge-source-v0.2.0/frontend/config.js
@@ -1,5 +1,5 @@
 window.NLM_BRIDGE_CONFIG = {
   GOOGLE_CLIENT_ID: "REPLACE_WITH_GOOGLE_OAUTH_WEB_CLIENT_ID",
-  APPS_SCRIPT_URL: "REPLACE_WITH_APPS_SCRIPT_WEB_APP_URL",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbynWKaVwG1SRE6uWJ6d4r0Q5wEvKbB5foIuphQBGDwi8P2r2qaP6K0FRAV8krr9R70P/exec",
   EXTENSION_ID: "kkbmofmmonolffejdkfnpjdckahlefob"
 };
