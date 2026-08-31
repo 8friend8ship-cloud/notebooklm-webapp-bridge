62795a2fa70f56391b14e4046c38fb6ef44e9f84
8friend8ship-cloud <homedesigntaedi@gmail.com>
2026-08-31 22:11:31 +0900

    test: enforce read-only central runner startup gate

--- tests/central-runner-notebooklm-bound-readonly.test.ps1
+++ tests/central-runner-notebooklm-bound-readonly.test.ps1
@@ -30,6 +30,7 @@ if($r -match '&\s*\$claspCmd\.Source\s+push\b'){throw 'RECOVER_FORBIDDEN_CLASP_P
 if($r -match '&\s*\$claspCmd\.Source\s+deploy\b'){throw 'RECOVER_FORBIDDEN_CLASP_DEPLOY'}
 
 $j=Get-Content -LiteralPath $tasks -Raw -Encoding UTF8 | ConvertFrom-Json
+if($j.version -ne '20260831.7-readonly-startup-gate'){throw ('TASK_MANIFEST_VERSION_INVALID:'+ $j.version)}
 $t=@($j.tasks | Where-Object {$_.taskId -eq 'TASK_20260831_NOTEBOOKLM_BOUND_READONLY_RECOVERY_001'})
 if($t.Count -ne 1){throw ('TASK_COUNT_INVALID:'+ $t.Count)}
 $t=$t[0]
@@ -39,6 +40,24 @@ if($t.targetTitle -ne 'WEBAPP_TEMPLATE_03'){throw 'TASK_TITLE_INVALID'}
 if($t.expectedSpreadsheetId -ne '1TbQxEcCiiibu2-EmMGEdt79v4AUpE8JL2XrDEKeVRCk'){throw 'TASK_SPREADSHEET_INVALID'}
 if($t.expectedDeploymentId -ne 'AKfycbynWKaVwG1SRE6uWJ6d4r0Q5wEvKbB5foIuphQBGDwi8P2r2qaP6K0FRAV8krr9R70P'){throw 'TASK_DEPLOYMENT_INVALID'}
 if([int]$t.maxAttempts -ne 1){throw 'TASK_MAX_ATTEMPTS_INVALID'}
+
+$refresh=@($j.tasks | Where-Object {$_.taskId -eq 'TASK_20260831_CENTRAL_RUNNER_REFRESH_X5_READONLY_002'})
+if($refresh.Count -ne 1 -or -not$refresh[0].enabled){throw 'READONLY_REFRESH_NOT_ENABLED'}
+if($refresh[0].targetTitle -ne 'CENTRAL_RUNNER_REFRESH_X5_READONLY'){throw 'READONLY_REFRESH_TARGET_INVALID'}
+
+$mutationCapable=@($j.tasks | Where-Object {
+  $_.enabled -and (
+    $_.taskId -eq 'TASK_20260822_CONTENTOS_BOUND_SYNC_001' -or
+    $_.taskId -eq 'TASK_20260829_CHROME_FLOW_HEALTH_RECOVERY_002' -or
+    $_.action -eq 'TRAVEL_APPS_SCRIPT_REPAIR'
+  )
+})
+if($mutationCapable.Count -ne 0){throw ('BOOTSTRAP_MUTATION_TASK_ENABLED:'+ (($mutationCapable|ForEach-Object{$_.taskId}) -join '|'))}
+$enabled=@($j.tasks | Where-Object {$_.enabled})
+$enabledIds=@($enabled | ForEach-Object {[string]$_.taskId})
+$allowedEnabled=@('TASK_20260831_CENTRAL_RUNNER_REFRESH_X5_READONLY_002','TASK_20260831_NOTEBOOKLM_BOUND_READONLY_RECOVERY_001')
+foreach($id in $enabledIds){if($allowedEnabled -notcontains $id){throw ('UNEXPECTED_BOOTSTRAP_TASK_ENABLED:'+ $id)}}
+if($enabledIds.Count -ne 2){throw ('BOOTSTRAP_ENABLED_TASK_COUNT_INVALID:'+ $enabledIds.Count)}
 
 $rt=Get-Content -LiteralPath $runner -Raw -Encoding UTF8
 if(-not$rt.Contains("'BOUND_APPS_SCRIPT_READONLY_RECOVERY'")){throw 'RUNNER_ACTION_MISSING'}
